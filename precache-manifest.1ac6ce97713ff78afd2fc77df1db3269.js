827082dd50a4c5c93b4286b321e7736215ea49ea
PetaCoinVK <50578444+PetaCoinVK@users.noreply.github.com>
2019-06-03 00:47:02 +0800

    Add files via upload

---
+++ precache-manifest.1ac6ce97713ff78afd2fc77df1db3269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "aca68dc115604820faac",
+    "url": "/static/js/main.aca68dc1.chunk.js"
+  },
+  {
+    "revision": "ddd1fbbd31aa1531532e",
+    "url": "/static/js/2.ddd1fbbd.chunk.js"
+  },
+  {
+    "revision": "aca68dc115604820faac",
+    "url": "/static/css/main.d8ff6c6f.chunk.css"
+  },
+  {
+    "revision": "ddd1fbbd31aa1531532e",
+    "url": "/static/css/2.6ad46f84.chunk.css"
+  },
+  {
+    "revision": "0c2c7cfae2997e5acd0353fe513a16ff",
+    "url": "/index.html"
+  }
+];
